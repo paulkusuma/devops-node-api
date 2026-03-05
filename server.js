8ccbb27fa0839fce246392aa3f5acae9645f2fff
paulkusuma <paul.kusuma10@gmail.com>
2026-03-05 13:39:27 +0000

    test CI Pipeline

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node API! UPDATE444 BRANCH DEV');
+  res.send('HELLO DevOps CI TEST');
 });
 
 app.get('/health', (req, res) => {
